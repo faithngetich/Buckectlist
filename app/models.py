d72f4b51ac1355638daa62250d78be419f0c2296
faithngetich <faith.ngetich@andela.com>
2017-05-11 11:09:23 +0300

    [Feature] Add items table

--- app/models.py
+++ app/models.py
@@ -34,3 +34,23 @@ class Buckectlist(Base):
     user = relationship('Users')
     modified_date = Column(DateTime, nullable=False)
 
+class Item(Base):
+    """creates Items tables"""
+    __tablename__ = "Items"
+
+    id = Column(Integer, foreign_key=True, autoincrement=True)
+    buckectlist_id = Column(ForeignKey('bucketList.id'), nullable=False)
+    name = Column(String, nullable=False)
+    created_date = Column(DateTime, nullable=False)
+    modified_date = Column(DateTime, nullable=False)
+    buckectlist = relationship('Buckectlist')
+    Done = Column(Boolean)
+
+class DatabaseCreator(object):
+    """creating database connection to object"""
+    def __init__(self, db_name=None):
+        self.db_name = db_name + '.db'
+        self.engine = create_engine('sqlite:///' + self.db_name)
+        Base.metadata.create_all(self.engine)
+        session_maker = sessionmaker(bind=self.engine)
+        self.session = session_maker()
