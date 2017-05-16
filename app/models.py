e1bb10ca6a06508f4b832403e56f250e2c7af84e
faithngetich <faith.ngetich@andela.com>
2017-05-16 13:20:02 +0300

    change tables

--- app/models.py
+++ app/models.py
@@ -23,28 +23,36 @@ class Users(Base):
         return 'username{} email{}'.format(self.user_name,self.email)
 
 
-class Buckectlist(Base):
-    """creates Buckectlist tables"""
+class Bucketlist(Base):
+    """creates Bucketlist tables"""
     __tablename__ = "bucketList"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(ForeignKey('user.id'), nullable=False)
+    parent_id = Column(ForeignKey('buckectlist.id'))
+    name = Column(String, unique=True)
+    description = Column(String(512))
     item = Column(String, nullable=False)
     created_date = Column(DateTime, nullable=False)
     user = relationship('Users')
     modified_date = Column(DateTime, nullable=False)
+    done = Column(Boolean)
+    # reference to the same goal table
+    # list of subgoal
+    # This self-reference of sorts is how the hierarchical nature of goals is accomplished in the relational database 
+    parent = relationship(lambda:Bucketlist, remote_side=id, backref='sub_buckectlist')
 
-class Item(Base):
-    """creates Items tables"""
-    __tablename__ = "Items"
+# class Item(Base):
+#     """creates Items tables"""
+#     __tablename__ = "Items"
 
-    id = Column(Integer, foreign_key=True, autoincrement=True)
-    buckectlist_id = Column(ForeignKey('bucketList.id'), nullable=False)
-    name = Column(String, nullable=False)
-    created_date = Column(DateTime, nullable=False)
-    modified_date = Column(DateTime, nullable=False)
-    buckectlist = relationship('Buckectlist')
-    Done = Column(Boolean)
+#     id = Column(Integer, foreign_key=True, autoincrement=True)
+#     bucketlist_id = Column(ForeignKey('bucketList.id'), nullable=False)
+#     name = Column(String, nullable=False)
+#     created_date = Column(DateTime, nullable=False)
+#     modified_date = Column(DateTime, nullable=False)
+#     buckectlist = relationship('Bucketlist')
+#     Done = Column(Boolean)
 
 class DatabaseCreator(object):
     """creating database connection to object"""
