b4a46dbdb00642244392de0217ac94c7239c0c89
faithngetich <faith.ngetich@andela.com>
2017-05-30 13:15:37 +0300

    [chore] Add a route code

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import jwt
 from flask import Flask
 from flask_jwt import JWT
+from flask import render_template
 
 from .config import config_by_name
 from .auth.auth import identity, authenticate
@@ -17,6 +18,10 @@ def create_app(config_name):
 
     from app.routes import routes as routes_blueprint
     app.register_blueprint(routes_blueprint, url_prefix='/api/v1')
+
+    def index():
+        return render_template('index.html')
+    app.add_url_rule('/','index', index)
 
     jwt.authentication_callback = authenticate
     jwt.identity_callback = identity
